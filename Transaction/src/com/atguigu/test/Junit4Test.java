d6d0166b8fc994600afb704f078759e4ad1e309c
hubo <1947390185@qq.com>
2020-09-05 19:02:46 +0800

    测试

--- Transaction/src/com/atguigu/test/Junit4Test.java
+++ Transaction/src/com/atguigu/test/Junit4Test.java
@@ -1,7 +1,7 @@
 package com.atguigu.test;
 
 import com.atguigu.config.RealConfig;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
